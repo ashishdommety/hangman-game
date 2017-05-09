c54ff8071ddc9dfa6ef14141afe9a738c7c2392b
ashishdommety <ashishuiux@gmail.com>
2017-05-08 20:11:43 -0700

    Fully functional! Still need to add sound effects and other jazzy stuff!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,130 +1,292 @@
+//getting the level text
+var levelInfo = document.getElementById("level");
+//getting the div that contains the blanks in order to append values
+var blank = document.getElementById("blanks");
+//getting the guesses remaining
+var guess = document.getElementById('guess');
+//getting space for wrong letters entered
+var wrongLetters = document.getElementById('wrongLetters');
+
+level = 1;
 //sample words
-var word = "hello";
+var words = ["hello", "greetings", "namaste"];
 
-// splitting the string into an array
-var letters = word.split("");
+// to check the word number
+var wordNo = 0;
 
-//getting all the letters from the dom.
-var one = document.getElementById("letter1");
-var two = document.getElementById("letter2");
-var three = document.getElementById("letter3");
-var four = document.getElementById("letter4");
-var five = document.getElementById("letter5");
-
-//creating the counter
+//counter for each time a letter is wrongly guessed
 var counter = 0;
 
-// hang man draw
-var hanger = 0;
-
-//empty array
-var checkAlphabets = [];
-
-// array of other alphabets
-var otherAlphabets = ["a", "b", "c", "d", "f", "g", "i", "j", "k", "m", "n", "p", "q", "r", "s",
-  "t", "u", "v", "w", "x", "y", "z"];
-
-//key functions to check for word "hello"
-document.onkeyup = function(key) {
-  if (key.key === "h") {
-    one.innerHTML = "H";
-    // one.style.backgroundColor = "yellow";
-    checkWin();
+//creates blanks for any word in the array
+function createBlanks() {
+  wrongLetters.innerHTML = "";
+  blank.innerHTML = "";
+  var counter = 0;
+  for (var i = 0; i < words[wordNo].length; i++) {
+    var div = document.createElement("div");
+    div.innerHTML = "_";
+    div.className = "letter";
+    blank.appendChild(div);
   }
-  if (key.key === "e") {
-    two.innerHTML = "E";
-    // two.style.backgroundColor = "yellow";
-    checkWin();
-  }
-  if (key.key === "l") {
-    three.innerHTML = "L";
-    four.innerHTML = "L";
-    // three.style.backgroundColor = "yellow";
-    // four.style.backgroundColor = "yellow";
-    checkWin();
-  }
-  if (key.key === "o") {
-    five.innerHTML = "O";
-    // five.style.backgroundColor = "yellow";
-    checkWin();
-  }
-  //-----working logic!
-  // else if(key.key === "a" ||key.key === "b" ||key.key === "c" ||key.key === "d"
-  // ||key.key === "f" ||key.key === "g" ||key.key === "i" ||key.key === "j"
-  // ||key.key === "k"||key.key === "m"||key.key === "n"||key.key === "p"
-  // ||key.key === "q"||key.key === "r"||key.key === "s"||key.key === "t"
-  // ||key.key === "u"||key.key === "v"||key.key === "w"||key.key === "x"
-  // ||key.key === "y"||key.key === "z"){
-  //   console.log(counter);
-  //   counter++;
-  //   checkLose();
-  // }
+}
 
+createBlanks();
 
-  //for loop that iterates through the otherAlphabets array
-  for (var j = 0; j < otherAlphabets.length; j++) {
-    //checks if the key pressed is a part of the otherAlphabets
-    if (key.key === otherAlphabets[j]) {
-
-      //if it is then splice it out and store it in alph
-      var alph = otherAlphabets.splice(j, 1);
-      //push alph into the checkAlphabets array
-      checkAlphabets.push(alph[0]);
-
-      console.log(checkAlphabets);
-
-//----------- this line is supposed to console.log that I already entered a specific alphabet
-      // //for loop to iterate through checkAlphabets
-      // for (var k = 0; k < checkAlphabets.length; k++) {
-      //  //compares spliced value to the checkAlphabets array
-      //   if (checkAlphabets[k] == alph) {
-      //     console.log("You already entered this alphabet:" + alph);
-      //   }
-      // }
+//HELLO
+function wordIsHello() {
+  document.onkeyup = function(press) {
+    // debugger;
+    if (press.key === words[wordNo].charAt(0)) {
+      var pos = words[wordNo].indexOf("h");
+      // console.log(pos);
+      document.getElementsByClassName("letter")[pos].innerHTML = press.key;
+      checkHello();
+    } else if (press.key === words[wordNo].charAt(1)) {
+      var pos = words[wordNo].indexOf("e");
+      // console.log(pos);
+      document.getElementsByClassName("letter")[pos].innerHTML = press.key;
+      checkHello();
+    } else if (press.key === words[wordNo].charAt(2)) {
+      var pos = words[wordNo].indexOf("l");
+      // console.log(pos);
+      document.getElementsByClassName("letter")[pos].innerHTML = press.key;
+      document.getElementsByClassName("letter")[pos + 1].innerHTML = press.key;
+    } else if (press.key === words[wordNo].charAt(4)) {
+      var pos = words[wordNo].indexOf("o");
+      // console.log(pos);
+      document.getElementsByClassName("letter")[pos].innerHTML = press.key;
+      checkHello();
+    } else {
+      var p = document.createElement("p");
+      p.innerHTML = press.key;
+      p.className = "wrong";
+      wrongLetters.appendChild(p);
       counter++;
       checkLose();
     }
   }
 }
 
+//GREETINGS
+function wordIsGreetings() {
+  document.onkeyup = function(press) {
+    if (press.key === words[wordNo].charAt(0)) {
+      var pos = words[wordNo].indexOf("g");
+      // console.log(pos);
+      document.getElementsByClassName("letter")[pos].innerHTML = press.key;
+      document.getElementsByClassName("letter")[pos + 7].innerHTML = press.key;
+      checkGreetings();
+    } else if (press.key === words[wordNo].charAt(1)) {
+      var pos = words[wordNo].indexOf("r");
+      // console.log(pos);
+      document.getElementsByClassName("letter")[pos].innerHTML = press.key;
+      checkGreetings();
+    } else if (press.key === words[wordNo].charAt(2)) {
+      var pos = words[wordNo].indexOf("e");
+      // console.log(pos);
+      document.getElementsByClassName("letter")[pos].innerHTML = press.key;
+      document.getElementsByClassName("letter")[pos + 1].innerHTML = press.key;
+      checkGreetings();
+    } else if (press.key === words[wordNo].charAt(4)) {
+      var pos = words[wordNo].indexOf("t");
+      // console.log(pos);
+      document.getElementsByClassName("letter")[pos].innerHTML = press.key;
+      checkGreetings();
+    } else if (press.key === words[wordNo].charAt(5)) {
+      var pos = words[wordNo].indexOf("i");
+      // console.log(pos);
+      document.getElementsByClassName("letter")[pos].innerHTML = press.key;
+      checkGreetings();
+    } else if (press.key === words[wordNo].charAt(6)) {
+      var pos = words[wordNo].indexOf("n");
+      // console.log(pos);
+      document.getElementsByClassName("letter")[pos].innerHTML = press.key;
+      checkGreetings();
+    } else if (press.key === words[wordNo].charAt(8)) {
+      var pos = words[wordNo].indexOf("s");
+      // console.log(pos);
+      document.getElementsByClassName("letter")[pos].innerHTML = press.key;
+      checkGreetings();
+    } else {
+      var p = document.createElement("p");
+      p.innerHTML = press.key;
+      p.className = "wrong";
+      wrongLetters.appendChild(p);
+      counter++;
+      checkLose();
+    }
+  }
+}
+
+//NAMASTE
+function wordIsNamaste(){
+  document.onkeyup = function(press){
+    if (press.key === words[wordNo].charAt(0)) {
+      var pos = words[wordNo].indexOf("n");
+      // console.log(pos);
+      document.getElementsByClassName("letter")[pos].innerHTML = press.key;
+      checkNamaste();
+    }
+    else if (press.key === words[wordNo].charAt(1)) {
+      var pos = words[wordNo].indexOf("a");
+      // console.log(pos);
+      document.getElementsByClassName("letter")[pos].innerHTML = press.key;
+      document.getElementsByClassName("letter")[pos+2].innerHTML = press.key;
+      checkNamaste();
+    }
+    else if (press.key === words[wordNo].charAt(2)) {
+      var pos = words[wordNo].indexOf("m");
+      // console.log(pos);
+      document.getElementsByClassName("letter")[pos].innerHTML = press.key;
+      checkNamaste();
+    }
+    else if (press.key === words[wordNo].charAt(4)) {
+      var pos = words[wordNo].indexOf("s");
+      // console.log(pos);
+      document.getElementsByClassName("letter")[pos].innerHTML = press.key;
+      checkNamaste();
+    }
+    else if (press.key === words[wordNo].charAt(5)) {
+      var pos = words[wordNo].indexOf("t");
+      // console.log(pos);
+      document.getElementsByClassName("letter")[pos].innerHTML = press.key;
+      checkNamaste();
+    }
+    else if (press.key === words[wordNo].charAt(6)) {
+      var pos = words[wordNo].indexOf("e");
+      // console.log(pos);
+      document.getElementsByClassName("letter")[pos].innerHTML = press.key;
+      checkNamaste();
+    }
+    else{
+      var p = document.createElement("p");
+      p.innerHTML = press.key;
+      p.className = "wrong";
+      wrongLetters.appendChild(p);
+      counter++;
+      checkLose();
+    }
+  }
+}
+
+//check level
+function checkLevel() {
+  if (level == 2) {
+    guess.innerHTML = 8;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    wordIsGreetings();
+  }
+  if (level == 3) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    wordIsNamaste();
+  }
+}
+
+if (level == 1) {
+  wordIsHello();
+}
+
+
+function checkHello() {
+  if (document.getElementsByClassName("letter")[0].innerHTML == "h" &&
+    document.getElementsByClassName("letter")[1].innerHTML == "e" &&
+    document.getElementsByClassName("letter")[2].innerHTML == "l" &&
+    document.getElementsByClassName("letter")[4].innerHTML == "o") {
+
+    level++;
+    levelInfo.innerHTML = level;
+    // checkLevel();
+    wordNo++;
+    createBlanks();
+    // console.log("next level!");
+    alert("Hello!");
+    counter = 0;
+    console.log(counter);
+    checkLevel();
+    console.log(level);
+  }
+}
+function checkGreetings() {
+  if (document.getElementsByClassName("letter")[0].innerHTML == "g" &&
+    document.getElementsByClassName("letter")[1].innerHTML == "r" &&
+    document.getElementsByClassName("letter")[2].innerHTML == "e" &&
+    document.getElementsByClassName("letter")[4].innerHTML == "t" &&
+    document.getElementsByClassName("letter")[5].innerHTML == "i" &&
+    document.getElementsByClassName("letter")[6].innerHTML == "n" &&
+    document.getElementsByClassName("letter")[8].innerHTML == "s"
+  ){
+    level++;
+    levelInfo.innerHTML = level;
+    // checkLevel();
+    wordNo++;
+    createBlanks();
+    // console.log("next level!");
+    alert("Greetings!");
+    counter = 0;
+    console.log(counter);
+    checkLevel();
+    console.log(level);
+  }
+}
+function checkNamaste(){
+  if (document.getElementsByClassName("letter")[0].innerHTML == "n" &&
+    document.getElementsByClassName("letter")[1].innerHTML == "a" &&
+    document.getElementsByClassName("letter")[2].innerHTML == "m" &&
+    document.getElementsByClassName("letter")[4].innerHTML == "s" &&
+    document.getElementsByClassName("letter")[5].innerHTML == "t" &&
+    document.getElementsByClassName("letter")[6].innerHTML == "e"
+  ) {
+    levelInfo.innerHTML = "Congratulations! You won!";
+    // checkLevel();
+    wordNo++;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    winText();
+    createBlanks();
+    // console.log("next level!");
+    alert("Namaste!");
+    counter = 0;
+    console.log(counter);
+    checkLevel();
+    console.log(level);
+  }
+}
+
 function checkLose() {
   if (counter == 1) {
+    guess.innerHTML = 8 - counter;
     hang1();
   }
   if (counter == 2) {
+    guess.innerHTML = 8 - counter;
     hang2();
   }
   if (counter == 3) {
+    guess.innerHTML = 8 - counter;
     hang3();
   }
   if (counter == 4) {
+    guess.innerHTML = 8 - counter;
     hang4();
   }
   if (counter == 5) {
+    guess.innerHTML = 8 - counter;
     hang5();
   }
   if (counter == 6) {
+    guess.innerHTML = 8 - counter;
     hang6();
   }
   if (counter == 7) {
+    guess.innerHTML = 8 - counter;
     hang7();
   }
   if (counter == 8) {
+    guess.innerHTML = 8 - counter;
     hang8();
-  }
-
-}
-
-function checkWin() {
-  if (one.innerHTML == "H" && two.innerHTML == "E" &&
-    three.innerHTML == "L" && four.innerHTML == "L" && five.innerHTML == "O") {
-    var arr = [one, two, three, four, five];
-    for (var i = 0; i < arr.length; i++) {
-      arr[i].style.color = "red";
-    }
-    winText();
+    guess.style.color = "red";
   }
 }
+
 //canvas element for stick figure
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
@@ -182,14 +344,16 @@ function hang8() {
   ctx.moveTo(250, 225);
   ctx.lineTo(200, 270);
   ctx.stroke();
-
   //writing you lose
   ctx.font = "30px Arial";
   ctx.fillText("You Lose", 300, 250);
+  levelInfo.innerHTML = "Don't give up! Hit the refresh button to try again!";
 }
 
+//celebration text on the canvas
 function winText() {
-  //writing you lose
+  var celebration = ["Hooray!", "Yipee!", "Awesome!", "Great Job!"];
+  var win = Math.floor(Math.random() * celebration.length);
   ctx.font = "50px Arial";
-  ctx.fillText("You Win!", 300, 250);
+  ctx.fillText(celebration[win], 200, 250);
 }
